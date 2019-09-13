8f4115c473705d8cadcbfe2dd3c02c1925b60ce2
Brian Lewis <keller@csh.rit.edu>
2019-09-13 09:04:21 +0200

    Make the aperture tiny to bring more stuff into focus

--- src/bin/ch_11.rs
+++ src/bin/ch_11.rs
@@ -27,7 +27,7 @@ fn main() {
     let look_at = Vec3::new(0.0, 0.0, -1.0);
     let v_up = Vec3::new(0.0, 1.0, 0.0);
     let focus_dist = (&pos - &look_at).len();
-    let aperture = 5.0;
+    let aperture = 0.1;
     let camera = Camera::new(pos, look_at, v_up, 20.0, aspect, aperture, focus_dist);
 
     write!(f, "P3\n{} {}\n255\n", width, height).expect("couldn't write header");
